86d91d389052cba9f8c661facd3061290f12974c
eiliya <eiliya.abedini@gmail.com>
2025-07-20 21:06:31 +0200

    first release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "ir.act.personalAccountant"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 100
+        versionName = "1.00"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
